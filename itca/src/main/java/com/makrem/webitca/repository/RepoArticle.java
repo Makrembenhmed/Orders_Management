91265eb87fb1f20cac1ef6192206095f67358ca2
Makrembenhmed <128426617+Makrembenhmed@users.noreply.github.com>
2023-11-11 22:46:49 +0100

    Delete itca directory

--- itca/src/main/java/com/makrem/webitca/repository/RepoArticle.java
+++
@@ -1,9 +0,0 @@
-package com.makrem.webitca.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.makrem.webitca.models.Article;
-
-public interface RepoArticle extends PagingAndSortingRepository<Article, Long> {
-
-}
